3d774e6ed99b917b823eb42166cfd53b8c6b3e69
Phạm Phú Quý Đôn <donezombie@gmail.com>
2019-01-31 15:05:01 +0700

    .

--- Champ-info.js
+++ Champ-info.js
@@ -12,15 +12,26 @@ fetch(`https://universe-meeps.leagueoflegends.com/v1/vn_vn/champions/${nameChamp
     })
 })
 
+// https://champion.gg/champion/Ahri/Middle?
+
 var content = document.getElementById('content');
 function run(dataEachChampion) {
     var name = document.getElementById('name');
     var subName = document.getElementById('sub-name');
     var avatar = document.getElementById('avatar');
+    var right = document.getElementById('right');
+    var role = document.getElementById('role');
+    var quote = document.getElementById('quote');
+    var story = document.getElementById('story');
+    var howtoplay = document.getElementById('howtoplay');
     document.getElementById('body').style.backgroundImage = `url(${dataEachChampion.champion.image.uri})`;
     name.innerHTML = `${dataEachChampion.champion.name.toUpperCase()}`;
     subName.innerHTML = `${dataEachChampion.champion.title}`
     avatar.innerHTML = `<img class="image" src="${dataEachChampion.champion.image.uri}" alt="" srcset="">`
+    story.innerHTML = `${dataEachChampion.champion.biography.full}`;
+    role.innerHTML = `Vị Trí: ${dataEachChampion.champion.roles[0].name}`;
+    quote.innerHTML = `<blockquote>${dataEachChampion.champion.biography.quote}</blockquote>`;
+    howtoplay.innerHTML = `<a target="_blank" href="https://champion.gg/champion/${dataEachChampion.champion.name}"><button> How to play </button></a>`
     console.log(dataEachChampion);
 
 }
